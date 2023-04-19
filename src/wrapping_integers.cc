c679dcb7df576d1684e108af95b22c10602cf995
Zixuan Xu <419384252@qq.com>
2023-04-19 07:23:55 +0000

    finish part one

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <ostream>
+#include <sys/types.h>
 
 using namespace std;
 
@@ -22,48 +23,26 @@ uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
   // return {};
   //convert seqno(Wrap32) -> absolute seqno(uint64_t)
 
-  uint32_t q = checkpoint / (1UL << 32);
-  uint32_t remain = checkpoint % (1UL << 32);
-  uint32_t offset = raw_value_ - zero_point.raw_value_;
+  // uint32_t q = checkpoint / (1UL << 32);
+  // uint32_t remain = checkpoint % (1UL << 32);
+  // uint32_t offset = raw_value_ - zero_point.raw_value_;
 
-  if (offset > remain && (offset - remain) > (1UL<<32) - (offset - remain) && q > 0){
-    q--;
+  // if (offset > remain && (offset - remain) > (1UL<<32) - (offset - remain) && q > 0){
+  //   q--;
+  // }
+  // if (offset < remain && (remain - offset) > (1UL<<32)-((remain - offset)) && q < UINT32_MAX){
+  //   q++;
+  // }
+  // return q*(1UL<<32) + offset;
+
+  uint32_t check = checkpoint % (1UL << 32);
+  uint32_t abs_seq = raw_value_ - zero_point.raw_value_;
+  uint64_t ans = checkpoint + abs_seq - check;
+
+  if (ans + (1UL << 32) > checkpoint && ans + (1UL<<32) - checkpoint < (check - abs_seq)){
+    ans += (1UL << 32);
+  }else if (ans - (1UL << 32) < checkpoint && checkpoint - (ans - (1UL << 32)) < (abs_seq - check)){
+    ans -= (1UL << 32);
   }
-  if (offset < remain && (remain - offset) > (1UL<<32)-((remain - offset)) && q < UINT32_MAX){
-    q++;
-  }
-  return q*(1UL<<32) + offset;
-
-  
-
-  // Wrap32 check = wrap(checkpoint, zero_point);
-  // uint32_t dist = 0;
-  // uint64_t ans = 0;
-  // if(zero_point.raw_value_ == 19){
-  //   cout << "checkpoint: " << checkpoint<< endl;
-  //   cout << "check raw value: " << check.raw_value_<< endl;
-  //   cout << "raw value: " << raw_value_ << endl;
-  // }
-  // if (check.raw_value_ > raw_value_){
-  //   dist = check.raw_value_ - raw_value_;
-  //   if (dist < checkpoint){
-  //     ans = checkpoint - dist;
-  //   }else{
-  //     ans = checkpoint + (1UL << 32) -dist;
-  //   }
-  // }else if(check.raw_value_ < raw_value_){
-  //   dist = raw_value_ - check.raw_value_;
-  //   ans = checkpoint + dist;
-  // }else if(check.raw_value_ == raw_value_){
-  //   return checkpoint;
-  // }
-  // if (ans + (1UL << 32) - checkpoint < dist){
-  //   ans += (1UL << 32);
-  //   return ans;
-  // } 
-  // if (checkpoint - (ans - (1UL << 32)) < dist && ans >= (1UL << 32)){
-  //   ans -= (1UL << 32);
-  //   return ans;
-  // }
-  // return ans;
+  return ans;
 }
