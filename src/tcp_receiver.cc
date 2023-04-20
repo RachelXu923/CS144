da1c98a1aeef8dc1d9ede9d5bf98f3ad076715ec
Zixuan Xu <419384252@qq.com>
2023-04-20 00:07:28 +0000

    TCP receiver finished

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -1,18 +1,57 @@
 #include "tcp_receiver.hh"
+#include "tcp_receiver_message.hh"
+#include "wrapping_integers.hh"
+#include <cstddef>
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <optional>
+#include <string>
 
 using namespace std;
 
 void TCPReceiver::receive( TCPSenderMessage message, Reassembler& reassembler, Writer& inbound_stream )
 {
   // Your code here.
-  (void)message;
-  (void)reassembler;
-  (void)inbound_stream;
+  // (void)message;
+  // (void)reassembler;
+  // (void)inbound_stream;
+  if(!SYN && !message.SYN){
+    return;
+  }
+  uint64_t abs_seqno = 0;
+  if (message.SYN){
+    SYN = true;
+    ISN = message.seqno;
+    abs_seqno = message.seqno.unwrap(ISN, checkpoint) + 1;
+  }else{
+    abs_seqno = message.seqno.unwrap(ISN, checkpoint);
+  }
+  string str = message.payload.release();
+  cout << "payload: " << str << endl;
+  cout << "abs seqno: " << abs_seqno << endl;
+  reassembler.insert(abs_seqno-1, str, message.FIN, inbound_stream);
+  checkpoint += str.size();
+  cout << "check point: " << checkpoint << endl;
 }
 
 TCPReceiverMessage TCPReceiver::send( const Writer& inbound_stream ) const
 {
   // Your code here.
-  (void)inbound_stream;
-  return {};
+  // (void)inbound_stream;
+  TCPReceiverMessage msg;
+  if (inbound_stream.available_capacity() > UINT16_MAX){
+    msg.window_size = UINT16_MAX;
+  }else{
+    msg.window_size = inbound_stream.available_capacity();
+  }
+  if(!SYN){
+    return msg;
+  }
+  uint64_t written = inbound_stream.bytes_pushed() + 1;
+  if(inbound_stream.is_closed()){
+    written++;
+  }
+  msg.ackno = Wrap32::wrap(written, ISN);
+  return msg;
 }
--- src/tcp_receiver.hh
+++ src/tcp_receiver.hh
@@ -3,14 +3,21 @@
 #include "reassembler.hh"
 #include "tcp_receiver_message.hh"
 #include "tcp_sender_message.hh"
+#include "wrapping_integers.hh"
+#include <cstdint>
 
 class TCPReceiver
 {
+protected:
+  bool SYN = false;
+  Wrap32 ISN{0};
+  uint64_t checkpoint = 0;
 public:
   /*
    * The TCPReceiver receives TCPSenderMessages, inserting their payload into the Reassembler
    * at the correct stream index.
    */
+  TCPReceiver() = default;
   void receive( TCPSenderMessage message, Reassembler& reassembler, Writer& inbound_stream );
 
   /* The TCPReceiver sends TCPReceiverMessages back to the TCPSender. */
